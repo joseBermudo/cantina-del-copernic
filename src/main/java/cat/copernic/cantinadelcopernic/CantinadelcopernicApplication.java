491be8f9c0905d97ce5d2126e8feceed728598b5
=Jose Antonio Bermudo Olea <=bermudo.olea.jose.antonio@alumnat.copernic.cat>
2023-02-17 09:34:57 +0100

    Primer comit

---
+++ src/main/java/cat/copernic/cantinadelcopernic/CantinadelcopernicApplication.java
@@ -0,0 +1,13 @@
+package cat.copernic.cantinadelcopernic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CantinadelcopernicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CantinadelcopernicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/copernic/cantinadelcopernic/CantinadelcopernicApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.copernic.cantinadelcopernic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CantinadelcopernicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
