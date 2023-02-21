0a2aa72c145d19dff7ef780b0510f4210807b5e0
= <=>
2023-02-21 13:17:54 +0100

    agregar todavía más y más carpetes

--- src/main/java/cat/copernic/cantinadelcopernic/moduloDeudas/Deuda.java
+++ src/main/java/cat/copernic/cantinadelcopernic/moduloDeudas/Deuda.java
@@ -4,10 +4,17 @@
  */
 package cat.copernic.cantinadelcopernic.moduloDeudas;
 
+import java.util.Date;
+import lombok.Data;
+
 /**
  *
  * @author Enric
  */
+@Data
 public class Deuda {
-    
+    private float cantidad;
+    private String correo; //El id del usuario
+    private Date fechaDeLaDeuda;
+    private boolean pagado;
 }
