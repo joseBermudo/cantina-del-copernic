eb6ec091a7cb87ece1b74dae8cfcd6897e055463
=Andres Rojas <=rojas.alzate.andres@alumnat.copernic.cat>
2023-02-22 21:41:49 +0100

    modelo vista,controlador del modulo sugerencias

--- src/main/java/cat/copernic/cantinadelcopernic/moduloSugerencias/modelo/Sugerencia.java
+++ src/main/java/cat/copernic/cantinadelcopernic/moduloSugerencias/modelo/Sugerencia.java
@@ -2,12 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package cat.copernic.cantinadelcopernic.moduloSugerencias;
+package cat.copernic.cantinadelcopernic.moduloSugerencias.modelo;
+
+import lombok.Data;
 
 /**
  *
- * @author Enric
+ * @author andre
  */
+@Data
 public class Sugerencia {
     
+    private String titulo;
+    private String descripcion;
+    private Boolean leida;
 }
