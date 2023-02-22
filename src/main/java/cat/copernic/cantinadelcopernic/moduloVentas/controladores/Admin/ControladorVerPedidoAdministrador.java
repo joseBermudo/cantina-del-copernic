ac01f72e1e955ef9a82c4c38feed1347921a21c0
lopez.salvador.marcos <lopez.salvador.marcos@alumnat.copernic.cat>
2023-02-22 18:13:26 +0100

    acabado todos los html de ventas

---
+++ src/main/java/cat/copernic/cantinadelcopernic/moduloVentas/controladores/Admin/ControladorVerPedidoAdministrador.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cat.copernic.cantinadelcopernic.moduloVentas.controladores.Admin;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Enric
+ */
+@Controller
+public class ControladorVerPedidoAdministrador {
+
+    /*La interface Model d'Spring Boot ens permet transferir dades entre el controlador i la vista
+     */
+    @GetMapping("/crearDeuda")
+    public String inici(Model model) {
+
+        return "/paginasDeudas/crearDeuda"; //Retorna la pàgina iniciEnviarDades
+    }
+    
+    
+}
