70f6d1885578872556c9d6d9de1b7ba71e73a324
Marcos López Salvador <lopez.salvador.marcos@alumnat.copernic.cat>
2023-02-27 09:09:36 +0000

    6: Creación paquete DAO

---
+++ src/main/java/cat/copernic/cantinadelcopernic/moduloInventario/controladores/ControladorListadoDeContenedores.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cat.copernic.cantinadelcopernic.moduloInventario.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Enric
+ */
+
+@Controller
+public class ControladorListadoDeContenedores {
+    
+    
+    @GetMapping("/listadoDeContenedores")
+    public String inici(Model model) {
+        
+        
+        return "/paginasUtensilios/listadoDeContenedores";
+    }
+}
