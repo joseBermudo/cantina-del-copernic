cf13513a1dd485069dceba2cd4eb300aaf6b37cd
Enric Ledo Muntal <=>
2023-03-01 18:56:51 +0100

    Tasca 6A: Cambiar nom de la carpeta Tareas y tots els controladors

--- src/main/java/cat/copernic/cantinadelcopernic/moduloTareas/controladores/ControladorListarTareas.java
+++ src/main/java/cat/copernic/cantinadelcopernic/moduloTareas/controladores/ControladorListarTareas.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package cat.copernic.cantinadelcopernic.moduloLimpieza.controladores;
+package cat.copernic.cantinadelcopernic.moduloTareas.controladores;
 
 import cat.copernic.cantinadelcopernic.modelo.Tarea;
 import cat.copernic.cantinadelcopernic.modelo.TipoTarea;
