3c1ef2610bf89e3ec21b36852a2ba05453195cfa
=Andres Rojas <=rojas.alzate.andres@alumnat.copernic.cat>
2023-04-12 18:43:40 +0200

    10A: test no terminado 2

--- src/test/java/testListar/testListar.java
+++ src/test/java/testListar/testListar.java
@@ -9,17 +9,17 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+/*import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringRunner;*/
 
 /**
  *
  * @author andre
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@ContextConfiguration(locations = "application.properties")
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@ContextConfiguration(locations = "application.properties")
 public class testListar {
    
     @Autowired
