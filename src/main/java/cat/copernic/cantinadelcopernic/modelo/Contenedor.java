fa868aeb1fd6f2df07dc7a5c4e0c8fa763e209ae
lopez.salvador.marcos <lopez.salvador.marcos@alumnat.copernic.cat>
2023-04-14 09:33:04 +0200

    10B. JavaDOC Modelos Venta e Inventario

--- src/main/java/cat/copernic/cantinadelcopernic/modelo/Contenedor.java
+++ src/main/java/cat/copernic/cantinadelcopernic/modelo/Contenedor.java
@@ -23,6 +23,14 @@ import lombok.Data;
  *
  * @author marku
  */
+
+/**
+ *
+ * La clase Contenedor representa un contenedor en el que se almacenan
+ * utensilios.
+ *
+ * Esta clase está mapeada a la tabla "contenedor" en la base de datos.
+ */
 @Data
 @Entity
 @Table(name = "contenedor")
@@ -30,11 +38,21 @@ public class Contenedor implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    /**
+     *
+     * El identificador único del contenedor. No puede estar vacío y debe tener
+     * una longitud máxima de 5 caracteres.
+     */
     @Id
     @NotEmpty
     @Size(max = 5)
     private String idcontenedor;
-
+    /**
+     *
+     * La lista de utensilios almacenados en el contenedor. Cada contenedor
+     * puede tener múltiples utensilios asociados. Los utensilios asociados se
+     * eliminarán en cascada al eliminar el contenedor.
+     */
     @OneToMany(mappedBy = "contenedor", cascade = CascadeType.REMOVE)
     private List<Utensilio> utensilios = new ArrayList<>();
 }
--- src/main/java/cat/copernic/cantinadelcopernic/modelo/Pedido.java
+++ src/main/java/cat/copernic/cantinadelcopernic/modelo/Pedido.java
@@ -4,7 +4,6 @@
  */
 package cat.copernic.cantinadelcopernic.modelo;
 
-import jakarta.persistence.CascadeType;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,16 +13,20 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.io.Serializable;
-
 
 import lombok.Data;
 
 /**
  *
  * @author marku
+ */
+/**
+ *
+ * La clase Pedido representa un pedido realizado por un profesor en el sistema.
+ *
+ * Esta clase está mapeada a la tabla "pedido" en la base de datos.
  */
 @Data
 @Entity
@@ -32,26 +35,36 @@ public class Pedido implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    /**
+     *
+     * El identificador único del pedido.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id_pedido;
-
-
-    
+    /**
+     *
+     * La bebida solicitada en el pedido. Cada pedido debe tener una bebida
+     * asociada.
+     */
     @ManyToOne()
-    @JoinColumn(name = "bebida_idbebida") 
+    @JoinColumn(name = "bebida_idbebida")
     @NotNull
     private Bebida bebida;
-
-
-    
+    /**
+     *
+     * El bocadillo de la semana solicitado en el pedido. Cada pedido debe tener
+     * un bocadillo de la semana asociado.
+     */
     @ManyToOne()
-    @JoinColumn(name = "bocadillo_semana_idbocadillo_semana") 
+    @JoinColumn(name = "bocadillo_semana_idbocadillo_semana")
     @NotNull
     private BocadilloSemana bocadilloSemana;
-
-
-    
+    /**
+     *
+     * El profesor que realizó el pedido. Cada pedido debe tener un profesor
+     * asociado.
+     */
     @ManyToOne()
     @JoinColumn(name = "usuarios_correo")
     @NotNull
