4d1dfea01b6c97e43a63f008354b3b66f9d55e64
Marcos López Salvador <lopez.salvador.marcos@alumnat.copernic.cat>
2023-04-16 19:39:22 +0000

    nbactions.xml

--- src/main/java/cat/copernic/cantinadelcopernic/modelo/Utensilio.java
+++ src/main/java/cat/copernic/cantinadelcopernic/modelo/Utensilio.java
@@ -4,7 +4,6 @@
  */
 package cat.copernic.cantinadelcopernic.modelo;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
